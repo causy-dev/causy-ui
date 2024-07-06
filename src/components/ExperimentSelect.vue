74bca2d90d592c7abb018299d7af6be762158b1b
Lilith Wittmann <mail@lilithwittmann.de>
2024-07-06 11:03:58 +0200

    fix(select): remove ugly effects in safari

--- src/components/ExperimentSelect.vue
+++ src/components/ExperimentSelect.vue
@@ -49,11 +49,18 @@ export default {
 
 <style scoped>
 .experiment-select {
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
   padding: 8px;
+  padding-right: 20px;
   border-radius: 5px;
+  background: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' fill='%239368e6'><polygon points='0,0 100,0 50,50'/></svg>")  no-repeat 95% 60%;
+  background-size: 8px 10px;
   background-color: #333;
   color: #fff;
   border-color: #333;
   font-weight: 600;
+
 }
 </style>
